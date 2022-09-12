27128c7a27bb40e560dcb21fab78ccb470857142
Max von Danwitz <max.danwitz@unibw.de>
2022-09-12 11:40:03 +0200

    Make temperature plot independent

--- Plot/PlotTemperature.py
+++ Plot/PlotTemperature.py
@@ -9,14 +9,61 @@ Created on Wed Jun 29 07:47:59 2022
 """
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
-import Plot as plot
+def LinesBW(df, x, y, title, xlabel, ylabel ='', saveAs=''):
+
+    import matplotlib.dates as mdates
+
+    from matplotlib.ticker import MaxNLocator
+
+    formatter = mdates.DateFormatter('%d')
+
+    cm = 1/2.54
+    plt.rcParams['figure.figsize'] =  (5.5*cm, 3.8*cm)
+
+    plt.rcParams['font.sans-serif'] = "Arial"
+    plt.rcParams.update({'font.size': 8})
+
+    colors = ['black', 'black' ,'grey', 'grey']
+    linestyles = ["--", "-", "--", "-"]
+
+    fig, ax = plt.subplots()
+
+    ax = df.plot(x=x, y=y, subplots=False, color = colors, style=linestyles, x_compat=True)
+
+
+    if xlabel != '':
+        ax.set_xlabel(xlabel)
+
+    if ylabel != '':
+        ax.set_ylabel(ylabel)
+
+    if len(y) == 1:
+        ax.set_ylabel(y[0])
+        ax.get_legend().remove()
+
+    plt.setp( ax.xaxis.get_majorticklabels(), rotation=35, ha="right", rotation_mode="anchor")
+
+    ax.legend(['Tmin Airport Munich', 'Tmin Test Bridge', 'Tmax Airport Munich', 'Tmax Test Bridge'], bbox_to_anchor=(1.05, 0.85))
+
+    ax.xaxis.set_major_locator(MaxNLocator(10))
+    ax.xaxis.set_major_formatter(formatter)
+
+    fig = ax.get_figure()
+
+    if saveAs == '':
+        fig.show()
+    else:
+        fig.savefig(saveAs,bbox_inches="tight")
+
+    plt.show()
 
 titles = ['../Weather/TemperatureComparisonMUC',
           # add,
           # more,
           # files,
-          # 
+          #
           ]
 
 for title in titles:
@@ -25,8 +72,10 @@ for title in titles:
 
     df['day'] = pd.to_datetime(df['day'])
 
-    plot.Lines(df, x='day', y = ['TMinFH', 'TMinB', 'TMaxFH', 'TMaxB'], 
-               title = title, 
-               xlabel='Date',
+    LinesBW(df, x='day', y = ['TMinFH', 'TMinB', 'TMaxFH', 'TMaxB'],
+               title = title,
+               xlabel='Date in March 2022',
                ylabel='Temperature [deg. C]',
                saveAs=title + '.pdf')
+
+
