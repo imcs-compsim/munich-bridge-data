c9772fe390a30d67e2a290172626d0f2b42abad8
Max von Danwitz <max.danwitz@unibw.de>
2022-09-12 11:59:14 +0200

    Make force plot black and white

--- Plot/PlotForcesInReferenceState.py
+++ Plot/PlotForcesInReferenceState.py
@@ -14,6 +14,7 @@ import matplotlib.pyplot as plt
 
 import matplotlib.dates as mdates
 
+from matplotlib.ticker import MaxNLocator
 
 date = '2022-03-07'
 
@@ -24,6 +25,8 @@ suffix1 = 'ForceAt' + '100' + 'Hz'
 titles = ['../ReferenceState/ReferenceState',
           ]
 
+colors = ['black', 'grey']
+
 fig, ax = plt.subplots()
 
 for i, title in enumerate(titles):
@@ -32,13 +35,16 @@ for i, title in enumerate(titles):
 
     df = pd.read_csv(title + suffix1 + '.csv.zip')
     df['Time (-)'] = pd.to_datetime(df['Time (-)'])
-    df.plot(ax = ax, x='Time (-)', y=['FRC-01 (kN)','FRC-02 (kN)'], x_compat=True)
+    df.plot(ax = ax, x='Time (-)', y=['FRC-01 (kN)','FRC-02 (kN)'], color = colors, x_compat=True)
     ax.set_ylabel('FRC-01/2 [kN]')
 
+    ax.xaxis.set_major_locator(MaxNLocator(7))
     ax.xaxis.set_major_formatter(formatter)
     ax.xaxis.set_minor_formatter(formatter)
     plt.setp( ax.xaxis.get_ticklabels(), rotation=35, ha="right", rotation_mode="anchor")
     plt.setp( ax.xaxis.get_ticklabels(minor=True), rotation=35, ha="right", rotation_mode="anchor")
     ax.set_xlabel('Time on ' + date)
+
+    ax.legend(['FRC-01 [kN]', 'FRC-02 [kN]'])
 
 fig.savefig(title + 'Force' + '.pdf', bbox_inches="tight")
